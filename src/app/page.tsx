d21662459d835c56e91fa827ca02475b320a7571
root <root@localhost.localdomain>
2026-03-11 13:06:36 +0800

    fix: 创建客户端 Provider 组件解决 SessionProvider 错误

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-'use client'
-
-import { SessionProvider } from 'next-auth/react'
 import PostFeed from '@/components/PostFeed'
 
 export default function Home() {
-  return (
-    <SessionProvider>
-      <PostFeed />
-    </SessionProvider>
-  )
+  return <PostFeed />
 }
